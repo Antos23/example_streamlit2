c4af0b2a9ad3e9a1ebddefb6b025284f542fd03c
Antos23 <35873201+Antos23@users.noreply.github.com>
2022-05-04 22:03:12 +0200

    Update app.py

--- app.py
+++ app.py
@@ -117,8 +117,8 @@ if st.session_state.count<=6:
         st.write("<h3><bold>You ran out of tries!</bold></h3>", unsafe_allow_html=True)
     
     if st.session_state['user_info']['username'] in df.user.tolist():
-        df.loc[df.user == st.session_state['user_info']['username'], 'win'] += st.session_state['user']['win']
-        df.loc[df.user == st.session_state['user_info']['username'], 'loss'] += st.session_state['user']['loss']
+        df.loc[df.user == st.session_state['user_info']['username'], 'win'] += st.session_state['user_info']['win']
+        df.loc[df.user == st.session_state['user_info']['username'], 'loss'] += st.session_state['user_info']['loss']
     else:
         df = df.append({'user':st.session_state['user_info']['username'],
                         'win':st.session_state['user_info']['win'],
